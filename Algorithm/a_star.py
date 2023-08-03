20ec6b23fc6e96c32c77d346c0b4e25c4c91d70e
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-08-04 05:06:23 +0630

    added humal readable output

--- Algorithm/a_star.py
+++ Algorithm/a_star.py
@@ -56,6 +56,28 @@ def a_star(grid, start, goal):
 
     return None
 
+
+def print_grid(grid, col_digits=2):
+    n_rows = len(grid)
+    n_cols = len(grid[0])
+    result = (
+        ' ' * (col_digits + 1) + ''.join(str(i) for i in range(n_cols)) + '\n'
+        + ' ' * (col_digits + 1) + ''.join('-' for i in range(n_cols)) + '\n')
+    for i, row in enumerate(grid):
+        result += (
+            str(i).zfill(col_digits) + '|'
+            + ''.join(str(elem) for elem in row) + '\n')
+    return result
+
+
+def draw_path(grid,path):
+    for row, col in path:
+        grid[row][col] = "\033[91m*\033[0m"  # ANSI escape code for red color
+
+# Print the updated grid
+    for row in grid:
+        print(" ".join(str(cell) for cell in row))
+
 if __name__ == "__main__":
     # Example usage
     grid = [
@@ -65,11 +87,20 @@ if __name__ == "__main__":
         [0, 0, 1, 1, 0],
         [0, 0, 0, 0, 0]
     ]
-    start_node = Node(1, 0)
-    goal_node = Node(4, 4)
+
+    print(print_grid(grid))
+    
+    start_node = Node(0, 0)
+    goal_node = Node(2, 3)
 
     path = a_star(grid, start_node, goal_node)
+
+    sorted_path = [[x, y] for x, y in path]
+
     if path:
         print("Path found:", path)
+        print("\n")
+        draw_path(grid,sorted_path)
+
     else:
         print("Path not found.")
