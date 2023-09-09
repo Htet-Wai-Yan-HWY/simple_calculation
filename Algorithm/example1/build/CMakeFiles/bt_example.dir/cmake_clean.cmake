758ee8a90495929f692329b0e31d00047619cd96
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-09-09 21:58:05 +0630

    cctv

---
+++ Algorithm/example1/build/CMakeFiles/bt_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bt_example.dir/main.cpp.o"
+  "CMakeFiles/bt_example.dir/main.cpp.o.d"
+  "bt_example"
+  "bt_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bt_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
